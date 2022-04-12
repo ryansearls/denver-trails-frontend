16b87c09537aa8b0a48cb670924280f78a53244a
Ryan Searls <Ryan.searls@gmail.com>
2022-04-12 14:25:34 -0600

    allows to search by trail names usig Vue2filters

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from "./router";
 import axios from "axios";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Vue2Filters from "vue2-filters";
+
+Vue.use(Vue2Filters);
 
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
--- src/views/TrailsIndex.vue
+++ src/views/TrailsIndex.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="trails-index">
     <h1>{{ message }}</h1>
+    Search by title:
+    <input v-model="titleFilter" />
     <div class="row row-cols-1 row-cols-md-3 g-4">
-      <div class="col" v-for="trail in trails" :key="trail.id">
+      <div class="col" v-for="trail in filterBy(trails, titleFilter, 'name')" :key="trail.id">
         <div class="card h-100">
           <img :src="trail.image" class="card-img-top" alt="..." />
           <div class="card-body">
@@ -31,15 +33,17 @@
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 
 export default {
   data: function () {
     return {
       message: "Explore Denver Trails",
       trails: [],
+      titleFilter: "",
     };
   },
-
+  mixins: [Vue2Filters.mixin],
   created: function () {
     this.indexTrails();
   },
