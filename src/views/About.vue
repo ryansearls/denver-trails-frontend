28aa8f50825e3995efb599653210d2ec0f3e5682
Ryan Searls <Ryan.searls@gmail.com>
2022-05-09 19:48:45 -0600

    updates about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>Welcome to Colorado Trails!</h1>
-    <h2>The reason I created this site is to keep track of all the trails I've completed around the state.</h2>
+    <h2>This site details hikes around the state of Colorado.</h2>
     <h2>It also allows me to add my favorite hikes to a list.</h2>
   </div>
 </template>
