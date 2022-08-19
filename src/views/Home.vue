a3d256ef5a9a7a4c94865151f571385144edf4d0
Ryan Searls <Ryan.searls@gmail.com>
2022-08-19 13:05:10 -0600

    adds hike link on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <a href="https://dayhikesneardenver.com/10-best-hikes-visiting-colorado/" target="_blank">Top 10 Hikes near Denver</a>
   </div>
+
 </template>
 
 <style></style>
